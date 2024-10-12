ba0f1a16978faa448bd1a6224ff8e671b615ab9f
sioaeko <101755125+sioaeko@users.noreply.github.com>
2024-10-12 12:32:17 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,11 @@
 import { defineConfig } from 'vite';
-import { resolve } from 'path';
 
 export default defineConfig({
   build: {
     outDir: 'dist',
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-      },
-    },
   },
   server: {
     port: 3000,
   },
+  root: '.', // 루트 디렉토리 명시
 });
